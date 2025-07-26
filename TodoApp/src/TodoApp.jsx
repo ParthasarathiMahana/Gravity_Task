fe64ccd8903ac407cd99b666b1a14dd372733563
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2025-07-26 15:47:55 +0530

    empty folder created with required components as asked in question

---
+++ TodoApp/src/TodoApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function TodoApp() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>My Todo App</div>
+  )
+}
+
+export default TodoApp
